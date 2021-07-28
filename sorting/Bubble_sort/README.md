98978fd35e5e0f3e6285f1f7d1a917221ee1bf05
Farham Ahmad <u2012839@warwick.ac.uk>
2021-07-28 20:10:10 +0100

    tidying up

---
+++ sorting/Bubble_sort/README.md
@@ -0,0 +1,14 @@
+# Bubble sort
+
+Bubble sort consists of n rounds. On each round, the algorithm iterates
+through the elements of the array. Whenever two consecutive elements are found
+that are not in correct order, the algorithm swaps them.
+
+## Analysis
+#### Time complexity 
+- Best case: `O(n)`, when the list is already sorted
+- Worst case: `O(n^2)`, when the list is in reverse order
+- Average: `O(n^2)`
+
+#### Space complexity
+- `O(1)`, only one single additional memory space is required, e.g a temp variable
---
+++ sorting/Test.java
@@ -0,0 +1,19 @@
+import java.util.Random;
+import Merge_sort.MergeSort;
+
+public class Test{
+    public static void main(String[] args){
+        //generate an array of random integers
+        int n = 20;
+        Integer[] numbers = new Integer[n];
+        Random rand = new Random();
+        for(int i = 0; i < n; i++){
+            numbers[i] = (Integer) rand.nextInt(100);
+        }
+
+        // Merge sort
+        MergeSort<Integer> mergeSort = new MergeSort<>();
+        mergeSort.mergeSort(numbers, 0, numbers.length - 1);
+        System.out.println(java.util.Arrays.toString(numbers));
+    }
+}
