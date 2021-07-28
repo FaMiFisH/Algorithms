aa66b82290bdbc8db4e4a5eb8455742bbaa2c73c
Farham Ahmad <u2012839@warwick.ac.uk>
2021-07-28 13:47:52 +0100

    added sorting directory

---
+++ Sorting/Bubble_sort/BubbleSort.java
@@ -0,0 +1,26 @@
+public class BubbleSort<T extends Comparable<? super T>>{
+    public static void main(String args[]){
+        // Test the algorithm here
+
+        int[] testArr = {5,4,3,2,1};
+        bubbleSort(testArr);
+        
+    }
+
+    private void bubbleSort(T[] arr){
+        int n = arr.length;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                // if element at index i is greater than i+1
+                if(arr[j].compareTo(arr[j+1]) > 0){
+                    // swap the elements
+                    T temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+
+
+}
---
+++ Sorting/Bubble_sort/README.md
@@ -0,0 +1,15 @@
+# Bubble sort
+
+Bubble sort consists of n rounds. On each round, the algorithm iterates
+through the elements of the array. Whenever two consecutive elements are found
+that are not in correct order, the algorithm swaps them.
+
+## Analysis
+Best case time complexity: O(n)
+- When the list is already sorted
+
+Worst case time complexity: O(n^2)
+- When the list is in reverse order
+
+Space complexity: O(1)
+- Only one single additional memory space is required, e.g a temp variable
